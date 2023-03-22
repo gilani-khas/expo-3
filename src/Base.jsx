252147a98e56aa5ac5e27a105a5751ac568b797e
gilani-khas <gilani95316@gmail.com>
2023-03-22 16:43:55 +0300

    new

---
+++ src/Base.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <img
+        src='https://gazeta.a42.ru/uploads/4cf/4cf84580-54f0-11e7-809d-b50085e853ab.jpg'
+        alt=''
+      />
+    </>
+  );
+}
+
+export default App;
